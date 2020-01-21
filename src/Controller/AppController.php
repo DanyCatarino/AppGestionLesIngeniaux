12615e7439aad41a54a8187014e34125de82bffc
Dylann456 <ddufag-raumel@lerebours.org>
2020-01-21 11:32:35 +0100

    Test
    RequêtesAppController

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Atelier;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AppController extends AbstractController
 {
@@ -14,6 +16,24 @@ class AppController extends AbstractController
     {
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+        ]);
+    }
+    /**
+     * @Route("/ajoutAtelier",name="app_ajout")
+     */
+    public function ajoutAtelier(EntityManagerInterface $manager)
+    {
+        $atelier1 = new atelier();
+
+        $atelier1->setDescription("Modelisation/Impression 3D")
+        ->setStatut("En Cours")
+        ->setType("Trimestriel hebdo")
+        ->setPrix(195);
+        
+        $manager->persist($atelier1);
+        $manager->flush();
+        return $this->render('atelier/index.html.twig',['controller_name'=>'AppController',
+        
         ]);
     }
 }
